2149b91329b2005fca88d1976784e4cd78b45a61
Christian <chr123x@hotmail.com>
2016-01-07 14:10:41 +0100

    Update to feed to handle friend request and access

--- src/Feed.java
+++ src/Feed.java
@@ -3,8 +3,6 @@ public class Feed extends Thread{
 	final String feed = "/space/Feed";
 	
 	Template t1;
-	Template t2;
-	Template t3;
 	
 	public void run() 
 	{
@@ -15,13 +13,28 @@ public class Feed extends Thread{
 				System.out.println("Looking for some file...");
 				t1.get();
 				System.out.println("Found file " + t1.name);
-
-				t2 = new Template("/space/GeneralData/" + t1.name,t1.name);
-				System.out.println("Putting the file in its corresponding subfolder...");
-				t2.put();
-				t3 = new Template("/space/UserData/" + t1.name,t1.name);
-				System.out.println("Putting the file in its corresponding subfolder...");
-				t3.put();
+				
+				String[] parts = t1.name.split("_");
+				String[] last = parts[2].split("\\.");
+				
+				if(parts[0].equals("REQ")) {
+					Template t2 = new Template("/space/Users/" + last[0] + "/Request/" + parts[1], parts[1]);
+					t2.put();
+				}
+				else if(parts[0].equals("ACC")) {
+					Template t2 = new Template("/space/Users/" + last[0] + "/Access/" + parts[1], parts[1]);
+					t2.put();
+					Template t3 = new Template("/space/Users/" + parts[1] + "/Access/" + last[1], last[0]);
+					t3.put();
+					
+				} else {
+					Template t2 = new Template("/space/GeneralData/" + t1.name,t1.name);
+					System.out.println("Putting the file in its corresponding subfolder...");
+					t2.put();
+					Template t3 = new Template("/space/UserData/" + t1.name,t1.name);
+					System.out.println("Putting the file in its corresponding subfolder...");
+					t3.put();
+				}
 			}
 
 		} catch (Exception e) { e.printStackTrace(); }
