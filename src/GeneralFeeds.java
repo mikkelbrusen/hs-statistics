aa8e4a2dded53f03e749f1e1149ec09d8d8b6689
Lars Jensen <Larskofodjensens@hotmail.com>
2016-01-07 13:47:19 +0100

    indented code, sry… :P

--- src/GeneralFeeds.java
+++ src/GeneralFeeds.java
@@ -9,43 +9,43 @@ public class GeneralFeeds extends Thread{
 	Template t1;
 	Template t2;
 	Template t3;
-	
-		public void run(){
-			try{
-				while(true){
-					
-					// Template t1 represents "any file in the space folder"
-					t1 = new Template(feed,"?");
-					System.out.println("Looking for some file...");
-					t1.get();
-					System.out.println("Found file " + t1.name);
-					
-					String[] parts = t1.name.split("_");
-					String[] endPart = parts[3].split("\\.");
-					String winCon = endPart[0];
-					
-					
-					t2 = new Template(general,parts[1]);
-					System.out.println("Looking for some file...");
-					if(t2.existsWithPrefix()){
-					
+
+	public void run(){
+		try{
+			while(true){
+
+				// Template t1 represents "any file in the space folder"
+				t1 = new Template(feed,"?");
+				System.out.println("Looking for some file...");
+				t1.get();
+				System.out.println("Found file " + t1.name);
+
+				String[] parts = t1.name.split("_");
+				String[] endPart = parts[3].split("\\.");
+				String winCon = endPart[0];
+
+
+				t2 = new Template(general,parts[1]);
+				System.out.println("Looking for some file...");
+				if(t2.existsWithPrefix()){
+
 					String[] parts2 = t2.name.split("_");
-					
+
 					int winCount = Integer.parseInt(parts2[1]);
 					int lossCount = Integer.parseInt(parts2[2]);
-					
+
 					if(winCon.equals("Win")){
 						winCount++;
 					} else {
 						lossCount++;
 					}
-					
+
 					t3 = new Template("/space/General/" + parts[1] + "_" + winCount + "_" + lossCount,t2.name);
 					System.out.println("Putting the file in its corresponding subfolder...");
 					t3.put();
-					
-					} else {
-						
+
+				} else {
+
 					if(winCon.equals("Win")){
 						t3 = new Template("/space/General/" + parts[1] + "_" + 1 + "_" + 0,t2.name);
 						System.out.println("Putting the file in its corresponding subfolder...");
@@ -56,12 +56,12 @@ public class GeneralFeeds extends Thread{
 						t3.put();
 					}
 
-					
-					}	
-				}
-				
+
+				}	
 			}
-			catch (Exception e) { e.printStackTrace();
+
+		}
+		catch (Exception e) { e.printStackTrace();
 		}
 
 	}	
--- src/UserFeeds.java
+++ src/UserFeeds.java
@@ -9,44 +9,44 @@ public class UserFeeds extends Thread{
 	Template t1;
 	Template t2;
 	Template t3;
-	
-		public void run(){
-			try{
-				while(true){
-					
-					// Template t1 represents "any file in the space folder"
-					t1 = new Template(feed,"?");
-					System.out.println("Looking for some file...");
-					t1.get();
-					System.out.println("Found file " + t1.name);
-					
-					String[] parts = t1.name.split("_");
-					String[] endPart = parts[3].split("\\.");
-					String winCon = endPart[0];
-					
-					
-					t2 = new Template(users + "/" + parts[0] + "/Statistics",parts[1]);
-					System.out.println("Looking for some file...");
-					
-					if(t2.existsWithPrefix()){
-					
+
+	public void run(){
+		try{
+			while(true){
+
+				// Template t1 represents "any file in the space folder"
+				t1 = new Template(feed,"?");
+				System.out.println("Looking for some file...");
+				t1.get();
+				System.out.println("Found file " + t1.name);
+
+				String[] parts = t1.name.split("_");
+				String[] endPart = parts[3].split("\\.");
+				String winCon = endPart[0];
+
+
+				t2 = new Template(users + "/" + parts[0] + "/Statistics",parts[1]);
+				System.out.println("Looking for some file...");
+
+				if(t2.existsWithPrefix()){
+
 					String[] parts2 = t2.name.split("_");
-					
+
 					int winCount = Integer.parseInt(parts2[1]);
 					int lossCount = Integer.parseInt(parts2[2]);
-					
+
 					if(winCon.equals("Win")){
 						winCount++;
 					} else {
 						lossCount++;
 					}
-					
+
 					t3 = new Template("/space/Users/" + parts[0] + "/Statistics/" + parts[1] + "_" + winCount + "_" + lossCount,t2.name);
 					System.out.println("Putting the file in its corresponding subfolder...");
 					t3.put();
-					
-					} else {
-						
+
+				} else {
+
 					if(winCon.equals("Win")){
 						t3 = new Template("/space/Users/" + parts[0] + "/Statistics/" + parts[1] + "_" + 1 + "_" + 0,t2.name);
 						System.out.println("Putting the file in its corresponding subfolder...");
@@ -57,12 +57,12 @@ public class UserFeeds extends Thread{
 						t3.put();
 					}
 
-					
-					}	
-				}
-				
+
+				}	
 			}
-			catch (Exception e) { e.printStackTrace();
+
+		}
+		catch (Exception e) { e.printStackTrace();
 		}
 
 	}	
